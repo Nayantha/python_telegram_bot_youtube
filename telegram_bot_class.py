48f6e8a6c9c8fcf03e5dc41ea974587e1ab786ad
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-29 09:50:43 +0530

    add comments.

--- telegram_bot_class.py
+++ telegram_bot_class.py
@@ -7,8 +7,11 @@ from telegram.ext import ContextTypes
 BOT_TOKEN: Final = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 
+# Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome!...")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("youtube downloader")
+
+# Handle Responses
