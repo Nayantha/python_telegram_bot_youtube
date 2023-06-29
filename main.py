ec1a33d762dfe9086cab6b2e0111b9762937c875
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-29 09:44:16 +0530

    move telegram related env variables to telegram.py from main.py.

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-import os
-from typing import Final
 
-BOT_TOKEN: Final = os.getenv("BOT_TOKEN")
-BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 
---
+++ telegram.py
@@ -0,0 +1,5 @@
+import os
+from typing import Final
+
+BOT_TOKEN: Final = os.getenv("BOT_TOKEN")
+BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
