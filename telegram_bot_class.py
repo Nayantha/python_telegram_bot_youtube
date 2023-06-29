28a11e8f4ad19c70fffcbab472cc20c21832e18a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-29 09:48:08 +0530

    add a start command.

--- telegram_bot_class.py
+++ telegram_bot_class.py
@@ -2,9 +2,10 @@ import os
 from typing import Final
 
 from telegram import Update
+from telegram.ext import ContextTypes
 
 BOT_TOKEN: Final = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 
-async def start_command(update: Update,):
-    ...
+async def start_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Welcome!...")
