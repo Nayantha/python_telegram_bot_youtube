fe4ddfd97fc1113307295296bc736ad150871963
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-29 09:49:14 +0530

    add a helper command.

--- telegram_bot_class.py
+++ telegram_bot_class.py
@@ -7,5 +7,8 @@ from telegram.ext import ContextTypes
 BOT_TOKEN: Final = os.getenv("BOT_TOKEN")
 BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 
-async def start_command(update: Update,context: ContextTypes.DEFAULT_TYPE):
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Welcome!...")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("youtube downloader")
