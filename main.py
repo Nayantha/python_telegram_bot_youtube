38d664561d9c113311643d20d11b9b42b5fee893
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-29 09:16:52 +0530

    add variables to catch and store environmental variables.

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import os
+from typing import Final
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+BOT_TOKEN: Final = os.getenv("BOT_TOKEN")
+BOT_USERNAME: Final = os.getenv("BOT_USERNAME")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
